0eedf6e4fe8ee3a089c810c195245f0941958e5b
pete <peteward44@gmail.com>
2015-12-19 10:31:11 +0000

    added template tests

--- test/tests.js
+++ test/tests.js
@@ -12,19 +12,67 @@ var gs = require('..' );
 
 function defineTests( transport ) {
 
-	describe( transport.name + ' checkout', function() {
+	describe( transport.name + ' tests', function() {
 		this.timeout( 30 * 60 * 1000 ); // 30 minutes
 
-		helpers.promiseIt('CreateRepo', async function( tempDir ) {
+		helpers.promiseIt('createRepo', async function( tempDir ) {
 			let result = await helpers.createRepo( tempDir, transport );
 			assert.equal( transport.isRepoFolder( result.dir ), true, 'Repository directory created correctly' );
 		} );
 		
-		helpers.promiseIt('Checkout', async function( tempDir ) {
+		helpers.promiseIt('checkout', async function( tempDir ) {
 			let result = await helpers.createRepo( tempDir, transport );
 			let checkoutDir = path.join( tempDir, uuid.v4() );
 			await transport.checkout( result.url, null, checkoutDir );
 			assert.equal( transport.isWorkingCopy( checkoutDir ), true, 'Repository checkout out correctly' );
+		} );
+		
+		helpers.promiseIt('isWorkingCopyClean', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('update', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('exists', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('cat', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('unCat', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('getWorkingCopyInfo', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('getUrlHeadRevision', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('getWorkingCopyRevision', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('listTags', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('exportDir', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('createBranch', async function( tempDir ) {
+		
+		} );
+		
+		helpers.promiseIt('createTag', async function( tempDir ) {
+		
 		} );
 	} );
 }
